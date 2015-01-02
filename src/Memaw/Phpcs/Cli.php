a8684c1d2d0b2eed2fc9c225ad3b9a85692206aa
Memaw <maw@memaw.fr>
2015-01-02 15:12:04 +0100

    First commit with MVP library

---
+++ src/Memaw/Phpcs/Cli.php
@@ -0,0 +1,76 @@
+<?php
+namespace Memaw\Phpcs;
+
+class Cli
+{
+    /**
+     * Default values for phpcs
+     *
+     * @var array
+     */
+    protected $defaultValues = array();
+
+    /**
+     * Constructor
+     *
+     * @param array $defaultValues Default values for phpcs
+     */
+    public function __construct($defaultValues = array())
+    {
+        if (isset($defaultValues['configurationFile'])) {
+            $configurationFilename = $defaultValues['configurationFile'];
+        } elseif (file_exists(getcwd() . '/memaw-phpcs.php')) {
+            $configurationFilename = getcwd() . '/memaw-phpcs.php';
+        } elseif (file_exists(getcwd() . '/memaw-phpcs.php.dist')) {
+            $configurationFilename = getcwd() . '/memaw-phpcs.php.dist';
+        }
+        if (isset($configurationFilename)) {
+            $this->defaultValues = json_decode(require $configurationFilename, true);
+        }
+        if (! empty($defaultValues)) {
+            $this->defaultValues = array_merge($this->defaultValues, $defaultValues);
+        }
+        if (! isset($this->defaultValues['installed_paths'])) {
+            $this->defaultValues['installed_paths'] = $this->getMemawStandardsPath();
+        } else {
+            $this->defaultValues['installed_paths'] .= ',' . $this->getMemawStandardsPath();
+        }
+        if (! isset($this->defaultValues['standard'])) {
+            $this->defaultValues['standard'] = array('Memaw');
+        }
+    }
+
+    /**
+     * Run the coding style check
+     *
+     * @return int
+     */
+    public function process()
+    {
+        $this->setDefaultValues();
+        \PHP_CodeSniffer_Reporting::startTiming();
+        $phpcs = new \PHP_CodeSniffer_CLI();
+        $phpcs->checkRequirements();
+        return $phpcs->process($this->defaultValues);
+    }
+
+    /**
+     * Get the path of the additional standards directory
+     *
+     * @return string
+     */
+    public function getMemawStandardsPath()
+    {
+        return __DIR__ . '/Standards';
+    }
+
+    /**
+     * Set default values in context
+     *
+     * @return null
+     */
+    protected function setDefaultValues()
+    {
+        $GLOBALS['PHP_CODESNIFFER_CONFIG_DATA'] = $this->defaultValues;
+    }
+}
